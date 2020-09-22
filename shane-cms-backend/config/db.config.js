4e7e828a7407edc262ce75bb5c6c5a2952bcfdd5
Shaneciora <33504380+Shaneciora@users.noreply.github.com>
2020-09-22 13:37:25 -0700

    atlas stuff 2

--- shane-cms-backend/config/db.config.js
+++ shane-cms-backend/config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: "mongodb+srv://shaneciora:<thisisapassword123>@cluster0.pedcl.mongodb.net/<dbname>?retryWrites=true&w=majority"
+    url: "mongodb+srv://shaneciora:thisisapassword123@cluster0.pedcl.mongodb.net<web_cms_db?retryWrites=true&w=majority"
   };
